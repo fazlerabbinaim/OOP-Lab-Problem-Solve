4815e0000cd0cb729d14109057289c12770563c2
Fazle Rabbi Naim <mdfazlerabbi3278@gmail.com>
2026-07-28 12:17:37 +0600

    Week-2

---
+++ Week-2/parallelogram.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class parallelogram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        int h = sc.nextInt();
+
+        if( b <= 0 || h <= 0){
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+            return;
+        }
+        System.out.println(b*h);
+    }
+}
